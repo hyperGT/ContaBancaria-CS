89f71ef50fb6fe6ffee182cf6c8904ee1a468a17
hyperGT <charlesgabriel102@gmail.com>
2024-08-12 14:38:29 -0300

    segundo commit

--- ContaBancaria/obj/Debug/net8.0/ContaBancaria.AssemblyInfo.cs
+++ ContaBancaria/obj/Debug/net8.0/ContaBancaria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContaBancaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d4e33e728700e26b336d88cbee00551bad43fdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContaBancaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContaBancaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
